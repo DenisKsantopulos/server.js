1891db61868a3a6a58e8dec51ec37e1fc19c72e6
not-a-mimic-22 <denksant@gmail.com>
2023-05-06 11:44:12 +0300

    server

--- main.js
+++ main.js
@@ -3,28 +3,15 @@ const http = require('http');
 const PORT = 3000;
 const host='127.0.0.1';
 
-/* const express = require('express');
-const app = express();
-const stats = {};  
-app.use(express.json());
-app.use((req, res, next) => {
-    const userAgent = req.headers['user-agent'];
-    if (userAgent) {
-      stats[userAgent] = (stats[userAgent] || 0) + 1;
-    }
-    next();
-  }); */
-  
-
 let k=0;
 const stats = {};
-/* let userAgent=1; */
+
 const server = http.createServer((req,res)=>{
     console.log('Server request');   
     console.log(`listening port ${PORT}`);
     console.log(req.url, req.method); 
     if (req.method=='GET' && req.url === '/'){
-      /* console.log(req.method); */
+      
       res.write('Hello world!');
 
       res.end();
@@ -40,17 +27,29 @@ const server = http.createServer((req,res)=>{
             html += `<tr><td>${userAgent}</td><td>${k}</td></tr>`;
         }
         html += '</tbody></table>';
-        /* res.send(html); */
+    
         res.end(html);
     }
     if (req.method=='POST' && req.url === '/comments'){
         //POST
         res.setHeader('Content-Type','application/json')
+        let body = '';
+
+        req.on('data', (chunk) => {
+          body += chunk.toString();
+        });
+    
+        req.on('end', () => {
+          const comment = JSON.parse(body);
+          console.log('New comment:', comment);
+          res.end('Comment added successfully');
+        });
         const data = JSON.stringify([
         {
             "//first_comment":  "The first JSON comment.",
             "//second_comment": "The second JSON comment."
         }
+        
     ]) 
     
     res.end(data);
@@ -60,23 +59,6 @@ const server = http.createServer((req,res)=>{
     }
 })
 
-/* app.post("/", urlencodedParser, function (request, response) {
-    
-    const data = JSON.stringify([
-        {
-            "//first_comment":  "The first JSON comment.",
-            "//second_comment": "The second JSON comment."
-        }
-    ])
-    response.send(data);
-}); */
-
-/* req.on('error', (err) => {
-    // This prints the error message and stack trace to `stderr`.
-    console.error(err.stack);
-  }); */
-
 server.listen(PORT,host,(error)=>{
-    
     error ? console.log(error) : console.log('New request');
 })
